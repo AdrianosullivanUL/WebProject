e1b292aa5d7ab4e7632c94ae54d1808c4847eb39
AdrianosullivanUL <38805326+AdrianosullivanUL@users.noreply.github.com>
2018-06-14 08:18:17 +0100

    Added SampleCRUD project
    Extract from web showing a simple app to Create, Read, Update & Delete entries from a mySQL database

---
+++ Web/SampleCRUD/CreateTable_Employees.sql
@@ -0,0 +1,25 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  501958452
+ * Created: 14-Jun-2018
+ */
+
+
+    CREATE TABLE employees (
+
+        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+
+        name VARCHAR(100) NOT NULL,
+
+        address VARCHAR(255) NOT NULL,
+
+        salary INT(10) NOT NULL
+
+    );
+
+
+
