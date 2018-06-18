88d3278468025ec640dbb4c28fe12e5d8e11d453
Adrian-PC\Adrian <adrian.osullivan@piercom.ie>
2018-06-18 12:42:28 +0100

    renamed documents and moved temp images to archive folder

---
+++ Web/BaselineCRUD/config.php
@@ -0,0 +1,14 @@
+<?php
+    define('DB_SERVER', 'localhost');
+    define('DB_USERNAME', 'root');
+    define('DB_PASSWORD', '');
+    define('DB_NAME', 'demo'); 
+
+    // Attempt to connect to MySQL database
+    $db_connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+    // Check connection
+    if($db_connection === false){
+        die("ERROR: Problem connecting to database, error details: " . mysqli_connect_error());
+    }
+
