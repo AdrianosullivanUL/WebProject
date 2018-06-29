41c89daea34607295b305db1a8f4aeb202c81798
CorneliusBroderick <9119124@studentmail.ul.ie>
2018-06-29 17:28:59 +0100

    Fixed some typo errors.

--- Database Implementation/Schema - Deirdre.sql
+++ Database Implementation/Schema - Deirdre.sql
@@ -4,44 +4,46 @@ USE group05;
 -- -------------------------------------------------------
 -- Create Tables
 -- -------------------------------------------------------
- 
- 
 
-----Create the Interests Table
+-- Create the Interests Table
 drop table if exists interests;
 create table interests(
 interest_id int NOT NULL AUTO_INCREMENT,
 type Varchar(100) NOT NULL,
 description Varchar(200) NOT NULL,
 PRIMARY KEY(interest_id)
-);
+);    
 
+
+-- Create the User Interests Table
 drop table if exists user_interests;
 create table user_interests(
-interest_id int NOT NULL AUTO_INCREMENT,
+user_interests_id int NOT NULL AUTO_INCREMENT,
 type Varchar(100) NOT NULL,
-PRIMARY KEY(interest_id)
+PRIMARY KEY(user_interests_id)
 );
 
-----Create the Relationship_Type Table 
+-- Create the Relationship_Type Table 
 drop table if exists relationship_type; 
 create table relationship_type( 
 user_id int NOT NULL AUTO_INCREMENT, 
 relationship_type Varchar(100) NOT NULL, 
-relationship_interest_id    int
+relationship_interest_id    int,
 PRIMARY KEY(user_id),
-FOREIGN KEY(relationship_interest_id ) REFERENCES interests(interest_id)
+FOREIGN KEY(relationship_interest_id) REFERENCES interests(interest_id)
 );
 
------Create the CITY Table
-drop table if exits city table;
+-- Create the CITY Table
+drop table if exists city;
 create table city(
 user_id  int NOT NULL AUTO_INCREMENT,
 city Varchar(100) NOT NULL,
 county Varchar(100) NOT NULL,
 geo_x float(4.2),
 geo_y float(4.2),
-Primary Key(user_id);
+Primary Key(user_id)
+);
+    
 
 
 
