18eb63da1f794440bf93b580290d992679263f21
Cornelius Broderick <9119124@studentmail.ul.ie>
2018-07-08 17:51:29 +0100

    First attempt at logon

--- Web/Logon.php
+++ Web/Logon.php
@@ -1,7 +1,7 @@
  <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>logon</title>
+  <title>Logon Screen</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -11,7 +11,20 @@
 <body>
 
 <div class="container">
-  <h1>logon Screen</h1>
+  <h1>Login Details</h1>
+  
+  <form action="UpdateProfile.php" method="post">
+  <label><b>email address</b></label>
+  <input type="text" name="uname" placeholder="email address"><br><br>
+  <label><b>Password</b></label>
+  <input type="text" name="pass" placeholder="password"><br><br>
+  
+  <a href="ResetPassword.php">Forgot password</a><br><br>
+  
+  <button style="background-color: #6495ed;color: white;
+  " type="submit"><b>Login</b></button>
+  </form>
+  
 </div>
 
 </body>
