88b0fefbee042669624043f27eccc7a0f855d197
16230256 <39533070+16230256@users.noreply.github.com>
2018-07-11 21:24:48 +0100

    new Register screen and Profile screen

---
+++ Web/UpdateProfile2.php
@@ -0,0 +1,72 @@
+ <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Personal Details</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+</head>
+<body>
+<div class="container">
+    <h1>Personal Details2</h1>
+	
+	<form action="#" method="post">
+
+            <div class="form-control"> 
+                <label class="header">Profile Photo:</label>
+
+                <input id="image" type="file" name="profile_photo" placeholder="Photo" required="" capture>
+            </div>
+			<br/><br/><br/>
+			</br>
+			
+			
+			
+			</div>
+<!--Lets try and do hobbies-->
+     	<div class="form-group">
+		<p align="middle">
+		    <label for="hobbies">Hobbies</label>
+            
+        </div>
+			
+		<div class="checkbox">
+		<p align="middle">
+             <label><input type="checkbox" value="">Sport</label>
+        </div>
+        <div class="checkbox">
+		<p align="middle">
+             <label><input type="checkbox" value="">Music</label>
+        </div>
+		<div class="checkbox">
+		<p align="middle">
+             <label><input type="checkbox" value="">Outdoors</label>
+        </div>
+		<div class="checkbox">
+		<p align="middle">
+             <label><input type="checkbox" value="">Reading</label>
+        </div>
+        
+<!------ Include the above in your HEAD tag ---------->
+
+<
+        <p align="right">
+        
+        <input class="btn btn-default" type="submit" value="Save">
+
+        
+        </div>
+
+    </form>
+
+</div>
+</body>
+
+</html>
