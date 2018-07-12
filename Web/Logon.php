d7fec3adaa170ceef4d6b65a40b5c7bddbcbd913
Cornelius Broderick <9119124@studentmail.ul.ie>
2018-07-12 16:42:23 +0100

    Adding consistent image to logon screen

--- Web/Logon.php
+++ Web/Logon.php
@@ -13,10 +13,21 @@ if(isset($_POST['submitted']))
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
 <head>
-      <meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
-      <title>Login</title>
-      <link rel="STYLESHEET" type="text/css" href="style/fg_membersite.css" />
-      <script type='text/javascript' src='scripts/gen_validatorv31.js'></script>
+	<meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
+	<title>Login</title>
+	<link rel="STYLESHEET" type="text/css" href="style/fg_membersite.css" />
+	<script type='text/javascript' src='scripts/gen_validatorv31.js'></script>
+	<style>
+		body {
+			background-image:    url(images/backlit-bonding-casual-708392.jpg);
+			background-size:     cover;                      /* <------ */
+			background-repeat:   no-repeat;
+			background-position: center center;              /* optional, center the image */
+		}
+
+
+	</style>
+	  
 </head>
 <body>
 
