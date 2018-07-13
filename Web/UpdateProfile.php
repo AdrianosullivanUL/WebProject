6dc3843a59038f25cc239ea20acfdd9cb69cafe5
16230256 <39533070+16230256@users.noreply.github.com>
2018-07-13 19:43:57 +0100

    need help with SQL command to get details for Update Profile

--- Web/UpdateProfile.php
+++ Web/UpdateProfile.php
@@ -9,6 +9,25 @@
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>  
+        <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+           <script>                    
+  $( function() {
+    $( "#slider-range" ).slider({
+        echo "first part of slider"
+      range: true,
+      min: 18,
+      max: 120,
+      values: [ 18, 65 ],
+      slide: function( event, ui ) {
+        $( "#age" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+      }
+    });
+    $( "#age" ).val( $( "#slider-range" ).slider( "values", 0 ) +
+      " - " + $( "#slider-range" ).slider( "values", 1 ) );
+  } );
+ </script>
+  
         <style>
             body {
                 background-image:    url(images/backlit-bonding-casual-708392.jpg);
@@ -85,17 +104,22 @@
                  
                      <?php
                         $gender = "";
-                   $sql = "SELECT * FROM user_profile where id =" . $user_id . ";";
-               //    echo ("sql" . $sql);
+                 
+                      
+                      $sql = "SELECT * FROM user_profile  up1 join gender g1 on g1.id = up1.gender_id where up1.id = " . $user_id . ";";
+                      
+                
+                 echo ("sql" . $sql);
                    if ($result = mysqli_query($db_connection, $sql)) {
                         if (mysqli_num_rows($result) > 0) {
                             while ($row = mysqli_fetch_array($result)) {
-                                if ($row['id'] == $user_id) {
+                               // if ($row['id'] == $user_id) {
+                               if ($row['id'] == $user_id) {
                                //     echo ($row['first_name']);
                                                         echo('<div class="form-group">');
                       echo('<label for="genderInput">Gender</label>');
-                      echo('<input type="text" class="form-control" id="genderInput" placeholder="'.$row['gender_id'].'">');
-                       $gender = $row['gender_id'];
+                      echo('<input type="text" class="form-control" id="genderInput" placeholder="'.$row['gender_name'].'">');
+                       $gender = $row['gender_name'];
                     //     echo ($row['first_name']);
                      //    echo('</input>');
                    echo('</div>');
@@ -151,6 +175,7 @@
                         <label for="seekingAgeSelection">Seeking Age Profile</label>
                         <input type="range" min="1" max="100" value="50" class="slider" id="seekingAgeSelection">
                     </div>
+ 
                     <div class="form-group">
                         <label for="travelDistanceSelection">Distance I will travel</label>
                         <input type="range" min="1" max="100" value="50" class="slider" id="travelDistanceSelection">
@@ -158,7 +183,7 @@
 
                     <button type="submit" class="btn btn-primary">
                         <i class="fas fa-heart "></i> Love
-                    </button>
+                    </button>   
                     <button type="submit" class="btn btn-primary">
                         <i class="fab fa-cuttlefish "></i> Casual
                     </button>
