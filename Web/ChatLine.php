b018d45134ae348922f5a52ff77f295dfcbe385c
CorneliusBroderick <9119124@studentmail.ul.ie>
2018-07-13 13:27:31 +0100

    Adding start of chat web app.

--- Web/ChatLine.php
+++ Web/ChatLine.php
@@ -14,5 +14,27 @@
   <h1>chaline Screen</h1>
 </div>
 
+<div id="main">
+
+<h1 style="background-color: #6495ed;color: white;"> -online</h1>
+	<div class="output">
+
+
+	</div>
+
+<form method="post" action="send.php">
+<textarea name="msg" placeholder="Type to send message...."
+class="form-control"></textarea><br>
+<input type="submit" value="Send">
+</form>
+<br>
+<form action="logout.php">
+
+<input stype="width: 100%;background-color: #6495ed;color:
+    white;font-size: 20px;" type="submit" value="Logout">
+</form>
+
+</div>
+
 </body>
 </html> 
