d720889468026d55f4c60a9c65100eba7db5358b
Cornelius Broderick <9119124@studentmail.ul.ie>
2018-07-18 17:20:09 +0100

    Adding new chat to Database.

--- Web/Send.php
+++ Web/Send.php
@@ -1,10 +1,21 @@
 <?php
+	session_start();
+	// redirect to the logon screen if the user is not logged in
+	if ($_SESSION['user_logged_in'] == 0) {
+		header("Location: Logon.php");
+	}
+	require_once 'database_config.php';
+	include 'group05_library.php';
+	$user_id = $_SESSION['user_id'];
+	$matching_user_id = $_SESSION['matching_user_id'];
 
- session_start();
- $msg=$_POST['msg'];
- $name=$_SESSION['name']
- echo "Name is: " $name . " Message is: " . $msg;
- $sql="insert into user_communications(from_user_id,message) values('$name','$msg')";
- $result=$conn->query($sql);
- echo $result;
+	$msg=$_POST['msg'];
+	
+	echo "user_id user no. " . $user_id . "<br>";
+	echo "msg is " . $msg . "<br>";
+	echo "matching_user_id use no. " . $matching_user_id . "<br>";
+	$sql="insert into user_communications(from_user_id, to_user_id, message) values('$user_id', '$matching_user_id', '$msg')";
+	$result = execute_sql_query($db_connection, $sql);
+	echo $result;
+	header("Location: ChatLine.php");
 ?>
