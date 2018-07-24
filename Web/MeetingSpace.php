59b87b44947f4f2c85219638da1acd7013cd9608
Adrian-PC\Adrian <adrian.osullivan@piercom.ie>
2018-07-24 07:48:54 +0100

    added stylesheet external file and fixed links in meetingspace

--- Web/MeetingSpace.php
+++ Web/MeetingSpace.php
@@ -160,66 +160,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-        <style>
-            body {
-                background-image:    url(backlit-bonding-casual-708392.jpg);
-                background-size:     cover;                      /* <------ */
-                background-repeat:   no-repeat;
-                background-position: center center;              /* optional, center the image */
-            }
-            /* Add a black background color to the top navigation */
-            .topnav {
-                background-color: #333;
-                overflow: hidden;
-            }
-            /* Style the links inside the navigation bar */
-            .topnav a {
-                float: left;
-                color: #F0F8FF;
-                text-align: center;
-                padding: 14px 16px;
-                text-decoration: none;
-                font-size: 17px;
-            }
-            /* Change the color of links on hover */
-            .topnav a:hover {
-                background-color: #ddd;
-                color: grey;
-            }
-            /* Add a color to the active/current link */
-            .topnav a.active {
-                background-color: #A9A9A9;
-                color: white;
-            }
-            /* Right-aligned section inside the top navigation */
-            .topnav-right {
-                float: right;
-            }
-            iv.first {
-                opacity: 0.1;
-                filter: alpha(opacity=10); 
-            }
-            <!-- invisible radio button with image select -->
-            ul {
-                list-style: none;
-            }
-            li {
-                display: inline-block;
-                margin-right: 15px;
-            }
-            input {
-                visibility:hidden;
-            }
-            img {
-                cursor: pointer;
-            }
-            input:checked + label {
-                border:2px solid #f00;
-            } 
-            .stackem div {
-                width: 100%;
-            }
-        </style>
+        <link rel="stylesheet" href="StyleSheet.css">
     </head>
     <body>
         <?php
@@ -247,9 +188,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 </a>
                 <div class="topnav-right">
                                     <a href="UpdateProfile.php" title="Edit your User Profile"><img height="16" width="16"  src='/images/Edit.png'/>Edit Profile</a>
-                                    <a href="UpdateProfile.php" title="Find People"><img height="16" width="16"   src='/images/Find.png'/>Match Finder</a>
-                                    <a href="UpdateProfile.php" title="Remove your User Profile"><img height="16" width="16"  src='/images/Delete.png'/>Delete Profile</a>
-                                    <a href="UpdateProfile.php" title="Log out of the system"><img height="16" width="16"  src='/images/Logoff.png'/>Logoff</a>
+                                    <a href="MatchFind.php" title="Find People"><img height="16" width="16"   src='/images/Find.png'/>Match Finder</a>
+                                    <a href="RemoveAccount.php" title="Remove your User Profile"><img height="16" width="16"  src='/images/Delete.png'/>Delete Profile</a>
+                                    <a href="Logout.php" title="Log out of the system"><img height="16" width="16"  src='/images/Logoff.png'/>Logoff</a>
                 </div>
             </div>
             <div class="container">
