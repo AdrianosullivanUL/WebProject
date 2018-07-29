0c7b8b12951d4431d9a424a180fff25f1be96006
marycroninUL <38848771+marycroninUL@users.noreply.github.com>
2018-07-29 11:42:21 +0100

    cancel and format profile update 2

--- Web/UpdateProfile2.php
+++ Web/UpdateProfile2.php
@@ -49,7 +49,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         }
         // Validate text inputs 
         // --------------------
-         if (strlen($myBio) == 0) {
+        if (strlen($myBio) == 0) {
             $message = "Please provide a short bio for your profile";
             $valid = false;
         }
@@ -125,121 +125,135 @@ if ($result = mysqli_query($db_connection, $sql)) {
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <meta charset="UTF-8">
-        <title>Personal Details2</title>
-        <meta name="viewport" content="width = device-width, initial-scale = 1">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-        <script src = "https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity = "sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin = "anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>  
-        <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js">
-        </script>
+        <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+        <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="StyleSheet.css">
+
     </head>
     <body>
-        <div class="container-fluid">
+        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" name="challenge"  class="form-group" role="form" onSubmit="return submitForm()" AUTOCOMPLETE = "off" >        
             <div class="topnav">
                 <a class="active">UPDATE PROFILE 2</a>
                 <a href="MeetingSpace.php" title="Meeting Space">
                     <?php echo $firstname . " " . $surname ?>
-
                 </a>
                 <div class="topnav-right">
                     <a href="RemoveAccount.php" title="Remove your User Profile"><img height="16" width="16"  src='http://hive.csis.ul.ie/4065/group05/images/Delete.png'/>Delete Profile</a>
                     <a href="Logout.php" title="Log out of the system"><img height="16" width="16"  src='http://hive.csis.ul.ie/4065/group05/images/Logoff.png'/>Logoff</a>
                 </div>
             </div>
+            <div class="row">
+                <div class ="container">
+                    <div class="col-md-10 col-md-offset-1" >
+                        <form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="post" enctype="multipart/form-data">
+                            <fieldset class="landscape_nomargin" style="min-width: 0;padding:    .75em .75em .75em;margin:0 2px;border: 2px solid silver;margin-bottom: 0em;background-color:lavender; opacity: .9;">
+                                <legend style="border-bottom: none;width: inherit;;padding:inherit;" class="legend">Profile Update Part 2</legend>
 
-            <form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="post" enctype="multipart/form-data">
-                <?php
-                if (strlen($message) > 0) {
-                    echo "<div class='alert alert-danger'>";
-                    echo "<p>" . $message . "</p>";
-                    echo "</div>";
-                }
-                ?>
-                <div class="row">
-                    <div class="col-sm-5 container border border-primary rounded bg-light text-dark ">
-                        <h1>Personal Details Page 2</h1>
-                        <div class="topnav">
-                        </div>
-                        </br>
-                        <div class="container border border-primary rounded bg-light text-dark col-sm-6">
-                            <div class="form-group">
-                                <?php
-                                if (strlen($picture) > 0) {
-                                    //echo ("Current photo");
-                                    echo '<img name = "pictureInput" class="portrait rounded-circle" height="200" width="200" src="data:image/jpeg;base64,' . $picture . '"/><i></i>';
-                                } else {
-                                    // echo ("No photo uploaded");
-                                    echo ("<img class='portrait rounded-circle' height='200' width='200' src='http://hive.csis.ul.ie/4065/group05/images/camera-photo-7.png'/><i></i>");
-                                }
+                                <div class="form-group">
+                                    <div class="col-sm-8 col-md-8 col-lg-12 col-xs-10 mobileLabel" ></div>
+                                </div>
+                                <div class="col-sm-4 col-md-4 col-lg-4 col-xs-10 mobileLabel">
+                                    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" name="challenge"  class="form-horizontal" role="form" onSubmit="return submitForm()" AUTOCOMPLETE = "off" >
+                                        <fieldset class="landscape_nomargin" style="min-width: 0;padding:    .75em .625em .75em!important;margin:0 2px;border: 2px solid silver!important;margin-bottom: 10em;background-color:whitesmoke; opacity: .9;">
+                                            <legend style="border-bottom: none;width: inherit;padding:inherit;" class="legend">Photo</legend>
+                                            <div class="form-group">
+                                                <?php
+                                                if (strlen($picture) > 0) {
+                                                    //echo ("Current photo");
+                                                    echo '<img name = "pictureInput" class="portrait rounded-circle" height="200" width="200" src="data:image/jpeg;base64,' . $picture . '"/><i></i>';
+                                                } else {
+                                                    // echo ("No photo uploaded");
+                                                    echo ("<img class='portrait rounded-circle' height='200' width='200' src='http://hive.csis.ul.ie/4065/group05/images/camera-photo-7.png'/><i></i>");
+                                                }
 //        echo ("after photo shoot");
-                                echo ("<figcaption>" . $first_name . " " . $first_name . "</figcaption>");
-                                ?>
+                                                echo ("<figcaption>" . $first_name . " " . $first_name . "</figcaption>");
+                                                ?>
 
+                                            </div>
+                                            <br>
+                                            <br>
+                                            <br>
+                                            <br>
+                                            <br>
+                                            <br>
+                                            <br>
 
-                            </div>
-
-                            </br>
-                            <div class="form-group">
+                                            <div class="form-group">
                                 <label class="header">Profile Photo:</label>
                                 <input type="file" name="fileToUpload" id="fileToUpload">
                             </div>
-                        </div>           
-                        </br>
-                    </div>
-                    <div class="col-sm-3 container border border-primary rounded bg-light text-dark ">
+                                        </fieldset>
+                                </div>
+                                <div class="col-sm-4 col-md-4 col-lg-5 col-xs-10 mobileLabel" >
+                                    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" name="challenge"  class="form-horizontal" role="form" onSubmit="return submitForm()" AUTOCOMPLETE = "off" >
+                                        <fieldset class="landscape_nomargin" style="min-width: 0;padding:    .75em .75em .75em!important;margin:0 2px;border: 2px solid silver!important;margin-bottom: 10em;background-color:whitesmoke; opacity: .9;">
+                                            <legend style="border-bottom: none;width: inherit;padding:inherit;" class="legend">Profile Bio</legend>
+                                            <div class="form-group">
+                                                <div class="col-sm-12 col-md-12 col-lg-12 col-xs-10 mobileLabel">
+                                                    <textarea rows="18" cols="30" class="form-control" name="myBioInput"><?php echo $myBio; ?></textarea>
+                                                    <br>
+                                                    <br>
+                                                </div>
+                                            </div>
 
-                        <div class="form-group">
-                            <h3>Bio</h3>
-                            <textarea rows="15" cols="50" class="form-control" name="myBioInput"><?php echo $myBio; ?></textarea>
-                        </div>
-                    </div>
+                                        </fieldset>
+                                    </form>    
+                                </div>
+                                <!--// Get user Interests   -->
 
-                    <!--Lets try and do hobbies-->
+                                <div class="col-sm-3 col-md-3 col-lg-3 col-xs-10 mobileLabel">
+                                    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" name="challenge"  class="form-horizontal" role="form" onSubmit="return submitForm()" AUTOCOMPLETE = "off" >
+                                        <fieldset class="landscape_nomargin" style="min-width: 0;padding:    .75em .75em .75em!important;margin:0 2px;border: 2px solid silver!important;margin-bottom: 10em;background-color:whitesmoke; opacity: .9;">
+                                            <legend style="border-bottom: none;width: inherit;padding:inherit;" class="legend">Interests</legend>
+                                            <?php
+                                            $sql = "  SELECT ud1.id, ud1.description , ui1.user_id FROM interests ud1 "
+                                                    . " left join (select * from user_interests where user_id = " . $user_id . ") ui1 on ui1.interest_id = ud1.id ";
 
-                    <div class="col-sm-3 container border border-primary rounded bg-light text-dark ">
+                                            $result = execute_sql_query($db_connection, $sql);
+                                            if ($result == null) {
+                                                $message = "ERROR: No interests found " . $user_id;
+                                            } else {
+                                                $checkBoxNumber = 0;
+                                                while ($row = mysqli_fetch_array($result)) {
+                                                    if ($row['user_id'] != null)
+                                                        $isChecked = "checked";
+                                                    else
+                                                        $isChecked = "";
+                                                    echo "<div class='checkbox'>";
+                                                    echo "    <p align='middle>'";
+                                                    echo "        <label><input type='checkbox' name='check_list[" . $row['id'] . "]' " . $isChecked . "  value=''>" . $row['description'] . "</label>";
+                                                    echo "</div>";
+                                                }
+                                            }
+                                            ?>
+                                            <br>
+                                            <br>
+                                            <br>
+                                            <br>
+                                        </fieldset>
+                                    </form>   
+                                </div>
 
-                        <div class="form-group">
-                            <h3>Hobbies</h3>
-
-                        </div>
-                        <?php
-// Get user Interests
-// ------------------
-                        $sql = "  SELECT ud1.id, ud1.description , ui1.user_id FROM interests ud1 "
-                                . " left join (select * from user_interests where user_id = " . $user_id . ") ui1 on ui1.interest_id = ud1.id ";
-
-//echo ("sql" . $sql);
-                        $result = execute_sql_query($db_connection, $sql);
-                        if ($result == null) {
-                            $message = "ERROR: No interests found " . $user_id;
-                        } else {
-                            $checkBoxNumber = 0;
-                            while ($row = mysqli_fetch_array($result)) {
-                                if ($row['user_id'] != null)
-                                    $isChecked = "checked";
-                                else
-                                    $isChecked = "";
-                                echo "<div class='checkbox'>";
-                                echo "    <p align='middle>'";
-                                echo "        <label><input type='checkbox' name='check_list[" . $row['id'] . "]' " . $isChecked . "  value=''>" . $row['description'] . "</label>";
-                                echo "</div>";
-                            }
-                        }
-                        ?>
-                        <p align = "right">
-                            <button class="btn btn-primary" name="btnAction" type="submit" value="Save">Save</button>
-                        </p>
-
+                                <div class="col-sm-10 col-md-10 col-lg-12 col-xs-10" style = "text-align: right">
+                                    <div class="form-group">
+                                        <button class="btn btn-primary" name="btnAction" type="submit" value="Save">Save</button> 
+                                        <button class="btn btn-warning" name="btnAction" type="submit" value="Cancel">Cancel</button>
+                                    </div>
+                                </div>
+                            </fieldset>
                     </div>
                 </div>
-
-            </form>
-
-        </div>
+            </div>
     </body>
 
 </html>
