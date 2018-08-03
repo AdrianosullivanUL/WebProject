f7f04756ff706b091f81ec920f16356d9070b0be
marycroninUL <38848771+marycroninUL@users.noreply.github.com>
2018-08-03 07:28:55 +0100

    match

--- Web/ViewMatchProfile.php
+++ Web/ViewMatchProfile.php
@@ -239,6 +239,10 @@ if ($result = mysqli_query($db_connection, $sql)) {
         <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
         <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
         <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>  
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="StyleSheet.css">
 
@@ -272,16 +276,16 @@ if ($result = mysqli_query($db_connection, $sql)) {
                 </div>
             </div>
             <div id="Help" class="collapse container">
-            This is where you meet your potential partners. 
-            <br>It works as follows:<br>
-            When you add or update your profile, the system automatically finds matches for you and places these in the "Possible Matches" section. 
-            You can also add matches here by finding people in the Match Finder screen and selecting them as a maybe. The system matches expire after a month so click on "maybe" to keep them.
-            <br>If you are interested in someone, select their profile and click Like, this will put them into the "People who I Like" section, they will stay there until they also like you.
-            <br>If they also like you then they will move to your chat area and you can both chat then.
-            <br>People who have liked you show in "People who like me" and again if you lile them they move into the chat area.
-            <br>If you want to remove a profile from your page, click on goodbye and they are removed.
-            <br>Have you taken offence from someone? Click on Report and the system moderator will review their account and if required Suspend
-        </div>
+                This is where you view your selected match. 
+                <br>It works as follows:<br>
+                When you add or update your profile, the system automatically finds matches for you and places these in the "Possible Matches" section. 
+                You can also add matches here by finding people in the Match Finder screen and selecting them as a maybe. The system matches expire after a month so click on "maybe" to keep them.
+                <br>If you are interested in someone, select their profile and click Like, this will put them into the "People who I Like" section, they will stay there until they also like you.
+                <br>If they also like you then they will move to your chat area and you can both chat then.
+                <br>People who have liked you show in "People who like me" and again if you lile them they move into the chat area.
+                <br>If you want to remove a profile from your page, click on goodbye and they are removed.
+                <br>Have you taken offence from someone? Click on Report and the system moderator will review their account and if required Suspend
+            </div>
             <div class="col-md-10 col-md-offset-1" >
 
                 <fieldset class="landscape_nomargin" style="min-width: 0;padding:    .75em .625em .75em!important;margin:0 2px;border: 2px solid silver!important;margin-bottom: 10em;background-color:lavender; opacity: .8;">
